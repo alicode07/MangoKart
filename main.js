6593a8b04070d8665c239db78a60e34d0b917041
Mohd Faisal Ali <mfaisalali9898@gmail.com>
2024-04-21 05:11:14 +0530

    modified

--- main.js
+++ main.js
@@ -1,6 +1,54 @@
 const btn1=document.getElementById("btn1")
 const btn2=document.getElementById("btn2")
 const btn3=document.getElementById("btn3")
+const dynTrend=document.getElementById("dynTrend")
+const TrendProduct=[
+    
+    {
+        name:"Grapes",
+        img:"./assets/grapes.jpg"
+    },
+    {
+        name:"Pine",
+        img:"./assets/pine.jpg"
+    },
+    {
+        name:"Tomato",
+        img:"./assets/tomato.jpg"
+    }
+];
+// console.log(dynTrend)
+
+// console.log(TrendProduct[0].name)
+
+
+    
+function getData(){
+    return dynTrend.innerHTML=
+    `
+    <div class="bottomimg" style="margin-right: 10px;">
+               <div class="topData" >
+                  ${TrendProduct[0].name}
+               </div>
+                <img src="${TrendProduct[0].img}">
+             </div>
+    <div class="bottomimg" style="margin-right: 10px;">
+               <div class="topData" >
+                  ${TrendProduct[1].name}
+               </div>
+                <img src="${TrendProduct[1].img}">
+             </div>
+    <div class="bottomimg" style="margin-right: 10px;">
+               <div class="topData" >
+                  ${TrendProduct[2].name}
+               </div>
+                <img src="${TrendProduct[2].img}">
+             </div>
+    `
+} 
+
+ 
+
 
 
     btn1.addEventListener("click",()=>
@@ -10,6 +58,7 @@ const btn3=document.getElementById("btn3")
      btn2.style.backgroundColor="rgb(200, 193, 193)"
     
      btn3.style.backgroundColor="rgb(200, 193, 193)"
+     getData()
      
      
      
@@ -37,4 +86,7 @@ const btn3=document.getElementById("btn3")
      
      
 })
+
+ 
+
 
