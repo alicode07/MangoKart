4291af04954a70a764dd6a97bbbf3a60bd0b7b7b
Mohd Faisal Ali <mfaisalali9898@gmail.com>
2024-04-21 02:34:42 +0530

    modified

--- Home-Page/index.js
+++ Home-Page/index.js
@@ -60,3 +60,17 @@ signupbtn.addEventListener("click",()=>
 
 
 
+//treding section
+
+const btn1=document.getElementById("btn1")
+const btn2=document.getElementById("btn2")
+const btn3=document.getElementById("btn3")
+btn1.addEventListener("click",()=>
+{
+    console.log(btn1)
+})
+// console.log
+
+
+
+
